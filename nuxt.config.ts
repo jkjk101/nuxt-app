22056c4e6daa82ba2cf540e57ef926630c07fa44
jkjk101 <jksm101852@gmail.com>
2024-11-19 22:49:22 +0800

    🐳 chore(nuxt): baseurl config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
   app: {
-    baseURL: '/'
+    baseURL: '/nuxt-app/'
     //baseURL: '/nuxt-apphttps://jkjk101.github.io/nuxt-app'
   },
   runtimeConfig: {
